a726f734332f843563ab7bbacaa521fef3f8b3df
Abdul <abdulbasit34310@gmail.com>
2025-09-23 01:56:16 +0500

    dynamic routing in client comp

--- app/(user)/blog/[slug]/page.jsx
+++ app/(user)/blog/[slug]/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// Dynamic route example
 export default async function Blog(props) {
   const user = await props.params;
   return (
@@ -8,3 +8,19 @@ export default async function Blog(props) {
     </div>
   )
 }
+
+// --- Dynamic Routing in Client Component ---
+// "use client"
+
+// import { use } from "react";
+
+// const SingleProfilePost = (props) => {
+//   const user = use(props.params);
+//   //   console.log(user);
+
+//   return (
+//     <h1>
+//       user : {user.username}, PostId : {user.postId}
+//     </h1>
+//   );
+// };
